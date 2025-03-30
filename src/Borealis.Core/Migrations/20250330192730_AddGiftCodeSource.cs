dec33d2fa02a5fbfeaf08a340afee1844323cd20
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2025-03-30 21:28:08 +0200

    Added missing migration

---
+++ src/Borealis.Core/Migrations/20250330192730_AddGiftCodeSource.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Borealis.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGiftCodeSource : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Source",
+                table: "GiftCodes",
+                type: "TEXT",
+                maxLength: 128,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Source",
+                table: "GiftCodes");
+        }
+    }
+}
