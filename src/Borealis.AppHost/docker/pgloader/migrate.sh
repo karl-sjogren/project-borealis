20f5bd59e1cc184f00cbc2c9057249698e421005
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2025-08-24 14:42:09 +0200

    Updated build.yml

--- src/Borealis.AppHost/docker/pgloader/migrate.sh
+++ src/Borealis.AppHost/docker/pgloader/migrate.sh
@@ -7,6 +7,6 @@ fi
 
 pgloader --with "data only" --with "quote identifiers" --with "truncate" --after /sqlite/post-script.sql \
     sqlite:///sqlite/db/Borealis.db \
-    postgresql://postgres:postgres@borealis-postgres2:5432/borealis
+    postgresql://postgres:postgres@borealis-postgres:5432/borealis
 
 mv /sqlite/db/Borealis.db /sqlite/db/Borealis.db.imported
