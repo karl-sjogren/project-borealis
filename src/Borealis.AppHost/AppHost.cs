91db1fbe8a5256a2a04fe738188cb69828048346
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2026-03-17 12:10:05 +0100

    Gaaah...

--- src/Borealis.AppHost/AppHost.cs
+++ src/Borealis.AppHost/AppHost.cs
@@ -1,17 +1,25 @@
 using Borealis.AppHost.Extensions;
 
+#pragma warning disable ASPIRECOMPUTE003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var registry = builder.AddContainerRegistry("ghcr", "ghcr.io", "karl-sjogren/project-borealis");
 
 builder.SetupDockerCompose();
 
 var postgresdb = builder.AddDatabase();
 
-var migrations = builder.AddMigrationProject(postgresdb);
+var migrations = builder.AddMigrationProject(postgresdb)
+    .WithContainerRegistry(registry);
 
-var web = builder.AddWeb(postgresdb, migrations);
+var web = builder.AddWeb(postgresdb, migrations)
+    .WithContainerRegistry(registry);
 
 builder
     .AddProxyProject(web)
     .AddFrontend(web);
 
 builder.Build().Run();
+
+#pragma warning restore ASPIRECOMPUTE003 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
